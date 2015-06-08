a8a0da3bd78fad8fbf2ced39b7eefe095bf5de01
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-06-08 23:54:25 +0200

    Updated asset pipeline. Updated layout application.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,34 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// BEGIN BASE JS
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require plugins/jquery-1.8.2/jquery-1.8.2.min.js
+//= require plugins/jquery-ui-1.10.4/ui/minified/jquery-ui.min.js
+//= require plugins/bootstrap-3.2.0/js/bootstrap.min.js
+//= require plugins/slimscroll/jquery.slimscroll.min.js
+//= require plugins/jquery-cookie/jquery.cookie.js
+// END BASE JS
+//
+// BEGIN PAGE LEVEL JS
+//= require plugins/gritter/js/jquery.gritter.js
+//= require plugins/flot/jquery.flot.min.js
+//= require plugins/flot/jquery.flot.time.min.js
+//= require plugins/flot/jquery.flot.resize.min.js
+//= require plugins/flot/jquery.flot.pie.min.js
+//= require plugins/sparkline/jquery.sparkline.js
+//= require plugins/jquery-jvectormap/jquery-jvectormap-1.2.2.min.js
+//= require plugins/jquery-jvectormap/jquery-jvectormap-world-mill-en.js
+//= require plugins/bootstrap-datepicker/js/bootstrap-datepicker.js
+//= require js/dashboard.min.js
+//= require js/apps.min.js
+// END PAGE LEVEL JS
+//
 //= require_tree .
+
+$(document).ready(function() {
+  App.init();
+  Dashboard.init();
+});
