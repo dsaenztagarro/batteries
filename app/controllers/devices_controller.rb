d2c132a1c250e7b77517172c20d98e8eeb55c686
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-07-03 07:26:22 +0200

    wip

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,6 +1,12 @@
 class DevicesController < SimpleCrud::BaseController
   default_crud
 
+  def edit
+    3.times {
+    @device.properties.build
+    }
+  end
+
   private
 
   def device_params
--- app/models/device.rb
+++ app/models/device.rb
@@ -1,7 +1,7 @@
 class Device < ActiveRecord::Base
   validates :name, presence: true
-  has_many :properties, class: 'DeviceProperty'
+  has_many :properties, class_name: 'DeviceProperty'
 
-  accepts_nested_attributes_for :device_properties, allow_destroy: true
+  accepts_nested_attributes_for :properties, allow_destroy: true
 end
 
