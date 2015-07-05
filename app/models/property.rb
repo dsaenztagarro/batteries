14d0dfd89fa8fe3601e03e827141dae138597aef
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-07-05 12:50:29 +0200

    Fixed n+1 queries for properties

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,5 +3,5 @@ class Property < ActiveRecord::Base
   belongs_to :attributable, polymorphic: true
   belongs_to :property_type
 	delegate :name, to: :property_type, prefix: true
-	default_scope { joins(:property_type).order('property_types.name ASC') }
+	default_scope { joins(:property_type).includes(:property_type).order('property_types.name ASC') }
 end
