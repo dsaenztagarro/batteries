b9fdc7933a2593b4f8910f87d16935606bab596f
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-07-16 06:06:53 +0200

    Device category is mandatory

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,7 @@
 # Devices present at home
 class Device < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+  validates :category, presence: true
   belongs_to :category, class_name: 'DeviceCategory'
   has_many :properties, :as => :attributable, :dependent => :destroy
   accepts_nested_attributes_for :properties, allow_destroy: true
