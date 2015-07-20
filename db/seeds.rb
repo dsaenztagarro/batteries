fdd1262fc81d8f25a7d94dc8d0690b0db4d8e52e
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-07-20 19:41:49 +0200

    Added styles for authentication forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# This file should contain all the record creation needed to seed the database
+# with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the
+# db with db:setup).
 #
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.new(
-    :email                 => 'testuser@xxxxx.xxx',
-    :password              => '12345678',
-    :password_confirmation => '12345678'
+User.create!(
+  email:                 'testuser@xxxxx.xxx',
+  password:              '12345678',
+  password_confirmation: '12345678'
 )
-user.save!
