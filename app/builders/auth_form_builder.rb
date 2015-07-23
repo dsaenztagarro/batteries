966fc91ddbae1794c4e24288e15dc8ea5e4f1ac2
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-07-23 06:46:30 +0200

    Fixed password_field generation for auth form builder

--- app/builders/auth_form_builder.rb
+++ app/builders/auth_form_builder.rb
@@ -1,30 +1,30 @@
 class AuthFormBuilder < ActionView::Helpers::FormBuilder
-	%w(email_field password_field check_box submit).each do |field|
-  	alias_method "orig_#{field}".to_s, field.to_s
-	end
+  %w(email_field password_field check_box submit).each do |field|
+    alias_method "orig_#{field}".to_s, field.to_s
+  end
 
-	%w(email_field password_field).each do |field|
-		define_method field.to_s do |method, orig_options = {}|
-			options = decorate_html(method, orig_options)
-			div_control do
-				orig_email_field(method, options) + error_details(method)
-			end
-		end
-	end
+  %w(email_field password_field).each do |field|
+    define_method field.to_s do |method, orig_options = {}|
+      options = decorate_html(method, orig_options)
+    div_control do
+      send("orig_#{field}", method, options) + error_details(method)
+    end
+    end
+  end
 
-	def check_box(method, options = {})
-		@template.content_tag :div, class: 'checkbox m-b-20' do
-			('<label>' + orig_check_box(method) + I18n.t(method) + '</label>').html_safe
-		end
-	end
+  def check_box(method, options = {})
+    @template.content_tag :div, class: 'checkbox m-b-20' do
+      ('<label>' + orig_check_box(method) + I18n.t(method) + '</label>').html_safe
+    end
+  end
 
   def submit(value=nil, options={})
     @template.content_tag :div, class: 'login-buttons' do
-			options.merge!(class: 'btn btn-success btn-block btn-lg')
-			# Disable button when the form is submitted
-			data_options = options[:data] || {}
-			options[:data] = data_options.merge(disable_with: 'Please wait..')
-			orig_submit(value, options)
+      options.merge!(class: 'btn btn-success btn-block btn-lg')
+      # Disable button when the form is submitted
+      data_options = options[:data] || {}
+      options[:data] = data_options.merge(disable_with: 'Please wait..')
+      orig_submit(value, options)
     end
   end
 
@@ -38,8 +38,8 @@ class AuthFormBuilder < ActionView::Helpers::FormBuilder
 
   def decorate_html(method, options)
     error_class = object.errors.key?(method) ? 'parsley-error' : ''
-		options.merge(class: "form-control input-lg #{error_class}").
-			merge(placeholder: I18n.t(method))
+    options.merge(class: "form-control input-lg #{error_class}").
+      merge(placeholder: I18n.t(method))
   end
 
   def div_control(&block)
