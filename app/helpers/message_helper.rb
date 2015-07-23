35db4907e2372e2c12bfb3854e7733c3d588f27a
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-07-23 06:41:15 +0200

    Showing errors on login

--- app/helpers/message_helper.rb
+++ app/helpers/message_helper.rb
@@ -1,7 +1,21 @@
 module MessageHelper
-  %w(success info warning error).each do |level|
+  %w(success info warning danger).each do |level|
     define_method "alert_#{level}".to_s do |message|
       render 'shared/alert', level: level, message: message
+    end
+  end
+
+  def show_flash
+    flash.keys.inject([]) do |memo, key|
+      memo << send("alert_#{level_for(key)}", flash[key])
+    end.join.html_safe
+  end
+
+  def level_for(key)
+    case key.to_sym
+    when :notice then :success;
+    when :alert then :danger;
+    else key
     end
   end
 end
