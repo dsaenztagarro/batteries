85abdd80608f8383e132c5e3f098a9aab80062a1
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-09 10:59:38 +0100

    Deploy to vagrant with deployer user

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -1,4 +1,4 @@
-set :rails_env, 'vagrant'
-set :user, 'vagrant'
-server 'myfamilyweb.local', user: 'vagrant', roles: %w{app db web}
+set :rails_env, 'production'
+set :user, 'deployer'
+server 'myfamilyweb.local', user: 'deployer', roles: %w{app db web}
 set :ssh_options, {:forward_agent => true, keys: ['~/.vagrant.d/insecure_private_key']}
