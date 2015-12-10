ac828da8c01de56166b800782c82a4ffd440212a
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-10 11:35:15 +0100

    Update production config

--- db/schema.rb
+++ db/schema.rb
@@ -13,48 +13,51 @@
 
 ActiveRecord::Schema.define(version: 20150711165102) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "device_categories", force: :cascade do |t|
-    t.string "name", limit: 255
+    t.string "name"
   end
 
   create_table "devices", force: :cascade do |t|
-    t.string   "name",                limit: 255
-    t.integer  "device_category_id",  limit: 4
-    t.string   "avatar_file_name",    limit: 255
-    t.string   "avatar_content_type", limit: 255
-    t.integer  "avatar_file_size",    limit: 4
+    t.string   "name"
+    t.integer  "device_category_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
   end
 
   create_table "properties", force: :cascade do |t|
-    t.string   "value",             limit: 255
-    t.integer  "property_type_id",  limit: 4
-    t.integer  "attributable_id",   limit: 4
-    t.string   "attributable_type", limit: 255
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.string   "value"
+    t.integer  "property_type_id"
+    t.integer  "attributable_id"
+    t.string   "attributable_type"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   add_index "properties", ["attributable_type", "attributable_id"], name: "index_properties_on_attributable_type_and_attributable_id", using: :btree
   add_index "properties", ["property_type_id"], name: "index_properties_on_property_type_id", using: :btree
 
   create_table "property_types", force: :cascade do |t|
-    t.string "name", limit: 255
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
