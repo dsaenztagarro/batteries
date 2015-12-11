3332853916e0ec5c89efc599725abe471128b0e1
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-11 18:18:15 +0100

    Refactoring views of battery_pack_types

--- app/controllers/battery_pack_types_controller.rb
+++ app/controllers/battery_pack_types_controller.rb
@@ -1,2 +1,13 @@
-class BatteryPackTypesController < ApplicationController
+class BatteryPackTypesController < SimpleCrud::BaseController
+  default_crud
+
+  private
+
+  def battery_pack_type_params
+    params.require(:battery_pack_type).permit(
+      :description,
+      :name,
+      :size
+    )
+  end
 end
--- app/models/battery_pack_type.rb
+++ app/models/battery_pack_type.rb
@@ -1,2 +1,6 @@
+# Represents a type of battery pack
 class BatteryPackType < ActiveRecord::Base
+  validates_presence_of :name, :description, :size
+  validates_uniqueness_of :name
+  validates_numericality_of :size, on: :create
 end
