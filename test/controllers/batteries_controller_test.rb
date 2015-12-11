532f202c815eedf0322846a4cf5d0eb4a9d104f1
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-12 00:14:19 +0100

    Fixed rubocop issues

--- test/controllers/batteries_controller_test.rb
+++ test/controllers/batteries_controller_test.rb
@@ -5,41 +5,41 @@ class BatteriesControllerTest < ActionController::TestCase
     @battery = batteries(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:batteries)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create battery" do
+  test 'should create battery' do
     assert_difference('Battery.count') do
-      post :create, battery: {  }
+      post :create, battery: {}
     end
 
     assert_redirected_to battery_path(assigns(:battery))
   end
 
-  test "should show battery" do
+  test 'should show battery' do
     get :show, id: @battery
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @battery
     assert_response :success
   end
 
-  test "should update battery" do
-    patch :update, id: @battery, battery: {  }
+  test 'should update battery' do
+    patch :update, id: @battery, battery: {}
     assert_redirected_to battery_path(assigns(:battery))
   end
 
-  test "should destroy battery" do
+  test 'should destroy battery' do
     assert_difference('Battery.count', -1) do
       delete :destroy, id: @battery
     end
--- test/controllers/battery_sizes_controller_test.rb
+++ test/controllers/battery_sizes_controller_test.rb
@@ -5,41 +5,41 @@ class BatterySizesControllerTest < ActionController::TestCase
     @battery_size = battery_sizes(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:battery_sizes)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create battery_size" do
+  test 'should create battery_size' do
     assert_difference('BatterySize.count') do
-      post :create, battery_size: {  }
+      post :create, battery_size: {}
     end
 
     assert_redirected_to battery_size_path(assigns(:battery_size))
   end
 
-  test "should show battery_size" do
+  test 'should show battery_size' do
     get :show, id: @battery_size
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @battery_size
     assert_response :success
   end
 
-  test "should update battery_size" do
-    patch :update, id: @battery_size, battery_size: {  }
+  test 'should update battery_size' do
+    patch :update, id: @battery_size, battery_size: {}
     assert_redirected_to battery_size_path(assigns(:battery_size))
   end
 
-  test "should destroy battery_size" do
+  test 'should destroy battery_size' do
     assert_difference('BatterySize.count', -1) do
       delete :destroy, id: @battery_size
     end
