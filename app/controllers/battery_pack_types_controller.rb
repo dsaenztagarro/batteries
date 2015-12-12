25bcf40ab6f6cc59fc5d496996990a314c8869ee
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-12 16:54:56 +0100

    Add cache to all battery pack types

--- app/controllers/battery_pack_types_controller.rb
+++ app/controllers/battery_pack_types_controller.rb
@@ -1,12 +1,21 @@
 class BatteryPackTypesController < SimpleCrud::BaseController
   default_crud
 
+  %w(after_destroy
+     after_save
+     after_update_attributes).each do |hook|
+    define_method hook do |result|
+      expire_fragment('all_battery_pack_types') if result
+    end
+  end
+
   private
 
   def battery_pack_type_params
     params.require(:battery_pack_type).permit(
       :description,
       :name,
+      :precharged,
       :size
     )
   end
