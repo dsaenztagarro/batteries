613e904ad71e428d8ad1401c2e7cc6db0c892fca
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-14 23:13:25 +0100

    Add database cleaner config

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,27 @@
+require 'database_cleaner'
+require 'factory_girl'
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    FactoryGirl.lint
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+    Warden.test_reset!
+  end
+end
