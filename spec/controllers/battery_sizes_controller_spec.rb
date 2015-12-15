b33cf1cfa825520cdc13d1a55ecef4095c5f9e7e
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-16 00:19:06 +0100

    Add rspec controller test

--- spec/controllers/battery_sizes_controller_spec.rb
+++ spec/controllers/battery_sizes_controller_spec.rb
@@ -4,6 +4,12 @@ describe BatterySizesController do
   login_user
 
   describe 'GET index' do
+    it "assigns @battery_sizes" do
+      battery_size = BatterySize.create(name: 'AA')
+      get :index
+      expect(assigns(:battery_sizes)).to eq([battery_size])
+    end
+
     it 'renders the index template' do
       get :index
       expect(response).to render_template('index')
