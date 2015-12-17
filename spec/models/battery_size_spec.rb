4eefd9a674ca3ee913d767dbae2e372177bf749c
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-17 23:06:44 +0100

    Remove debug trace

--- spec/models/battery_size_spec.rb
+++ spec/models/battery_size_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
 
 describe BatterySize do
-  it 'has a valid factory' do
-    expect(build(:battery_size)).to be_valid
-  end
-
   it { is_expected.to validate_presence_of(:name) }
 end
