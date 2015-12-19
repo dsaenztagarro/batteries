4d2162ddb6a401bc5041271db665b3b430dddaa1
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-19 15:43:09 +0100

    Adde battery model rspec tests

--- app/models/battery_model.rb
+++ app/models/battery_model.rb
@@ -1,6 +1,7 @@
 class BatteryModel < ActiveRecord::Base
   belongs_to :battery_size
   validates_presence_of :battery_size_id, :capacity, :name, :rechargeable
+  validates_uniqueness_of :name
   delegate :name, to: :battery_size, prefix: true, allow_nil: true
 
   def fullname
---
+++ spec/models/battery_model_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe BatteryModel do
+  it { is_expected.to validate_presence_of(:battery_size_id) }
+  it { is_expected.to validate_presence_of(:capacity) }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:rechargeable) }
+  it { is_expected.to validate_uniqueness_of(:name) }
+
+  describe '#:ta
+end
