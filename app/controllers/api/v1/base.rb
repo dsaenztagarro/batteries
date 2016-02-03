2948451db9b62c53f31a7546c6d211d5b6b3fdc4
David Saenz <david.saenz.tagarro@gmail.com>
2016-02-04 00:46:39 +0100

    Hide swagger documentation path

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -3,7 +3,8 @@ module API
     class Base < Grape::API
       mount API::V1::Devices
       add_swagger_documentation api_version: 'v1',
-                                mount_path: '/swagger_doc/v1'
+                                mount_path: '/swagger_doc/v1',
+                                hide_documentation_path: true
     end
   end
 end
